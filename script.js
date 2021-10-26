20b4ccd5f67fa26f827c1fe8e840665ce9c7aad8
CaioSunGarcia <csun.garcia@gmail.com>
2021-10-26 10:02:30 -0300

    Alteração de classe

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ timeline
     .to('.rock',3,{y:-200})
     .to('.girl', 3,{y:-200},'-=3')
     .fromTo('.bg1', 3,{y: -50},{y: 0, duration: 3}, '-=3')
-    .to('.content',3,{top:'0%'},'-=3');
+    .to('.content',3,{top:'0%'},'-=3')
+    .fromTo(".content-images", {opacity:0}, {opacity:1})
+    .fromTo(".text", {opacity:0}, {opacity:1});
  
     let scene = new ScrollMagic.Scene({
-        triggerElement: 'section',
+        triggerElement: "section",
         duration:"100%",
         triggerHook: 0,
     })
